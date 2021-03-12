7fddc52c9f64bd5dcbc11bb3c29944639473fb88
Chirrrag <csapra121@gmail.com>
2021-03-12 19:11:57 +0530

    Code of duplicate brackets

---
+++ DuplicateBrackets.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class DuplicateBrackets {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        
+        System.out.println(duplicate(str));
+    }
+    
+    public static boolean duplicate(String str) {
+        Stack<Character> st = new Stack<>();
+        
+        for(int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            
+            if(ch == ')') {
+                if(st.peek() == '(') {
+                    return true;
+                }
+                else {
+                    while(st.peek() != '(') {
+                        st.pop();
+                    }
+                    st.pop();
+                }
+            }
+            else {
+                st.push(ch);
+            }
+        }
+        
+        while(st.size() != 0) {
+            if(st.peek() == '(' )
+                return true;
+            st.pop();
+        }
+        return false;
+    }
+
+}
